9ecdc45bbe47c51058d9037fc90d64eda5b777de
Jeff-Russ <jeffreylynnruss@gmail.com>
2016-08-15 00:40:23 -0400

    major overhaul. project is nearly done

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
          
 if ( !function_exists( 'child_theme_configurator_css' ) ):
     function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_ext1', 'https://fonts.googleapis.com/css?family=Mate' );
+        wp_enqueue_style( 'chld_thm_cfg_ext1', 'http://fonts.googleapis.com/css?family=Mate' );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css' );
