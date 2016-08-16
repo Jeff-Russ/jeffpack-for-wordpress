10ad134453b8ccbab5ac8a8262065fc952f91398
Jeff-Russ <jeffreylynnruss@gmail.com>
2016-08-16 00:25:23 -0400

    add jquery and bootstrap

--- functions.php
+++ functions.php
@@ -2,6 +2,37 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+
+function enqueue_jquery_cdn() {
+    // https://css-tricks.com/snippets/wordpress/include-jquery-in-wordpress-theme/
+    if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+    function my_jquery_enqueue() {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery',
+            "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
+            false, null); // we may want to add a version number like: , array('jquery'), '1.10.2', true );
+        wp_enqueue_script('jquery');
+    }
+}
+function enqueue_bootstrap_js_cdn() {
+  wp_enqueue_script('bs-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true);
+}
+function enqueue_bootstrap_css_cdn() {
+  wp_enqueue_style('bs-css', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
+}
+
+function enqueue_bootstrap_js_edit() {
+  wp_enqueue_script('bs-js', get_stylesheet_directory_uri() . '/bootstraplet.js',array('jquery'), '', true );
+}
+function enqueue_bootstrap_css_edit() {
+  wp_enqueue_style('bs-css', get_stylesheet_directory_uri() . '/bootstraplet.css');
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_jquery_cdn');
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js_cdn'); // toggle _cdn <-> _edit
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css_edit');// toggle _cdn <-> _edit
+
+
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
