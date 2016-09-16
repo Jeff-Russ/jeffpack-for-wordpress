083de6f4e12e89a20268442f46e919a3e2313645
Jeff-Russ <jeffreylynnruss@gmail.com>
2016-09-16 18:38:54 -0400

    add guard against double defined functions between this theme and future plugins

--- functions.php
+++ functions.php
@@ -2,36 +2,58 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
-
-function enqueue_jquery_cdn() {
-    // https://css-tricks.com/snippets/wordpress/include-jquery-in-wordpress-theme/
+if ( !function_exists('enqueue_jquery_cdn') ){
+  add_action('wp_enqueue_scripts', 'enqueue_jquery_cdn');
+  function enqueue_jquery_cdn()
+  { // https://css-tricks.com/snippets/wordpress/include-jquery-in-wordpress-theme/
     if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
-    function my_jquery_enqueue() {
-        wp_deregister_script('jquery');
-        wp_register_script('jquery',
-            "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
-            false, null); // we may want to add a version number like: , array('jquery'), '1.10.2', true );
-        wp_enqueue_script('jquery');
+    function my_jquery_enqueue(){
+      wp_deregister_script('jquery');
+      wp_register_script('jquery',
+        "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "")
+        . "://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js",
+        false, null); // we may want to add a version number like: , array('jquery'), '1.10.2', true );
+      wp_enqueue_script('jquery');
     }
-}
-function enqueue_bootstrap_js_cdn() {
-  wp_enqueue_script('bs-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true);
-}
-function enqueue_bootstrap_css_cdn() {
-  wp_enqueue_style('bs-css', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
+  }
 }
 
-function enqueue_bootstrap_js_edit() {
-  wp_enqueue_script('bs-js', get_stylesheet_directory_uri() . '/bootstraplet.js',array('jquery'), '', true );
+// Add Bootstrap JS 
+if ( !function_exists('enqueue_bootstrap_js_cdn') ){
+  // toggle cdn option via comment/uncomment!
+  add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js_cdn');
+  function enqueue_bootstrap_js_cdn() {
+    wp_enqueue_script('bs-js',
+      '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js',
+      array('jquery'), true);
+  }
 }
-function enqueue_bootstrap_css_edit() {
-  wp_enqueue_style('bs-css', get_stylesheet_directory_uri() . '/bootstraplet.css');
+if ( !function_exists('enqueue_bootstrap_js_edit') ){
+  // toggle local edit option via comment/uncomment!
+  add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js_edit');
+  function enqueue_bootstrap_js_edit(){
+    wp_enqueue_script('bs-js', get_stylesheet_directory_uri()
+      .'/bootstraplet.js',array('jquery'), '', true );
+  }
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_jquery_cdn');
-add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js_cdn'); // toggle _cdn <-> _edit
-add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css_edit');// toggle _cdn <-> _edit
-
+// Add Bootstrap CSS
+if ( !function_exists('enqueue_bootstrap_css_edit') ){
+  // toggle local edit option via comment/uncomment!
+  add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css_edit');
+  function enqueue_bootstrap_css_edit(){
+    wp_enqueue_style('bs-css', get_stylesheet_directory_uri()
+      . '/bootstraplet.css');
+  }
+}
+if ( !function_exists('enqueue_bootstrap_css_cdn') ){
+  // toggle cdn option via comment/uncomment! 
+  add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css_cdn');
+  function enqueue_bootstrap_css_cdn(){
+    wp_enqueue_style('bs-css',
+      '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
+  }
+}
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
